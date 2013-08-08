de6c2c8e694419a69d8a46ea3db74570957244c4
unknown <oscarbralo@PC-OSCARB.eurotax.dns.com>
2013-08-08 09:30:17 +0200

    Bubble Sort

---
+++ BubbleSort/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BubbleSortDLL
+{
+    public class BubbleSort
+    {
+        public int[] bubbleSort(int[] input)
+        {
+            int temp = 0;
+            int x = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                x = 0;
+                for (int a = x + 1; a < input.Length - i; a++)
+                {
+                    if (input[x] > input[a])
+                    {
+                        temp = input[a];
+                        input[a] = input[x];
+                        input[x] = temp;
+                    }
+                    x++;
+                }
+            }
+            return input;
+        }
+    }
+}
