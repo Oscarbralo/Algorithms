b3a26ff7083388d7a52e4ab4271666d5899767f5
Oscarbralo <oscarbralo@outlook.com>
2013-09-19 17:14:25 +0200

    added the languages and CppInsertionSort

---
+++ CsharpInsertionSort/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class InsertionSort
+    {
+        public int[] iSort(int[] input)
+        {
+            for (int i = 1; i < input.Length; i++)
+            {
+                int key = input[i];
+                int j = i - 1;
+                while (j >= 0 && input[j] > key)
+                {
+                    input[j + 1] = input[j];
+                    j--;
+                }
+                input[j + 1] = key;
+            }
+            return input;
+        }
+    }
+
+    public class InsertionSortNonIncreasing
+    {
+        public int[] iSortNonIncreasing(int[] input)
+        {
+            for (int i = 1; i < input.Length; i++)
+            {
+                int key = input[i];
+                int j = i - 1;
+                while (j >= 0 && input[j] < key)
+                {
+                    input[j + 1] = input[j];
+                    j--;
+                }
+                input[j + 1] = key;
+            }
+            return input;
+        }
+    }
+}
