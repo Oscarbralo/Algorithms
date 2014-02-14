0462afc605fd675a458b5bf01b74ca9d2c7b4d8c
Oscarbralo <oscarbralo@outlook.com>
2014-02-14 20:19:14 +0100

    new algorithm added

---
+++ CSharpSelectionSort/SelectionSortClass.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace SelectionSort
+{
+    public class SelectionSortClass
+    {
+        public int[] Sort(int[] input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                int min = 1000000000;
+                int minIndex = -1;
+                for (int a = i + 1; a < input.Length; a++)
+                {
+                    if (input[a] < input[i] && input[a] < min)
+                    {
+                        min = input[a];
+                        minIndex = a;
+                    }
+                }
+                if (minIndex != -1)
+                {
+                    int temp = input[i];
+                    input[i] = input[minIndex];
+                    input[minIndex] = temp;
+                }
+            }
+            return input;
+        }
+    }
+}
