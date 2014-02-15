c5ef11934c6e676e59fd1990f95685b042f572cf
Oscarbralo <oscarbralo@outlook.com>
2014-02-15 16:03:23 +0100

    new algorithm added

---
+++ CSharpShellSort/ShellSortClass.cs
@@ -0,0 +1,29 @@
+﻿namespace ShellShort
+{
+    public class ShellSortClass
+    {
+        public int[] Sort(int[] input)
+        {
+            int totalLength = input.Length;
+
+            int hSort = 1;
+            while (hSort < totalLength / 3)
+                hSort = (3 * hSort) + 1;
+
+            while (hSort >= 1)
+            {
+                for (int i = hSort; i < totalLength; i++)
+                {
+                    for (int a = i; a >= hSort && (input[a] < input[a - hSort]); a -= hSort)
+                    {
+                        int temp = input[a];
+                        input[a] = input[a - hSort];
+                        input[a - hSort] = temp;
+                    }
+                }
+                hSort /= 3;
+            }
+            return input;
+        }
+    }
+}
